5e10763b2c345a7f6d88b5fc078692215594dd74
ipshitaRB <ipshitarburman@gmail.com>
2020-04-17 14:00:27 -0400

    Add textviews and buttons in MainActivity

--- app/src/main/java/com/droidrbi/tictactoe/MainActivity.java
+++ app/src/main/java/com/droidrbi/tictactoe/MainActivity.java
@@ -3,12 +3,38 @@ package com.droidrbi.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[][] buttons = new Button[3][3];
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+
+    private TextView player1ScoreTextView;
+    private TextView player2ScoreTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player1ScoreTextView = findViewById(R.id.player1Score);
+        player2ScoreTextView = findViewById(R.id.player2Score);
+
+        int i = 0, j = 0;
+        String buttonId = "";
+        String packageId = "id";
+        for(; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                buttonId = "gameButton" + i + j;
+
+                buttons[i][j] = findViewById(this.getResources().getIdentifier(buttonId,packageId,getPackageName()));
+
+            }
+        }
+
     }
 }
